44ae25dc00c5fc71f8dde52ca96bbc20b105def4
msm17b019 <sujeetkumariiit@gmail.com>
2021-03-23 17:37:34 +0530

    Python nptel area practice

---
+++ A9/calculate_area_3.py
@@ -0,0 +1,23 @@
+import imageio
+from PIL import Image 
+from random import *
+import random
+
+img=imageio.imread("area_image.PNG")
+count_tri=0
+count_sqr=0
+count=0
+while(count<=10000):
+    x=random.randint(0,754)
+    y=random.randint(0,1358)
+    z=0
+    if (img[x][y][z]==0):
+        count_sqr+=1
+        count+=1
+    else:
+        if(img[x][y][z]==91):
+            count_tri+=1
+            count+=1
+
+area_tri=(count_tri/count_sqr)*1
+print(area_tri)
