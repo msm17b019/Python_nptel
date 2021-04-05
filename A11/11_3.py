4c6258efc91248a04891037cac01119bd413cc09
msm17b019 <sujeetkumariiit@gmail.com>
2021-04-05 19:52:59 +0530

    commit my work

---
+++ A11/11_3.py
@@ -0,0 +1,23 @@
+n=int(input())
+
+def countStrings(n):
+  
+    a=[0 for i in range(n)]
+    b=[0 for i in range(n)]
+    a[0] = b[0] = 1
+    for i in range(1,n):
+        a[i] = a[i-1] + b[i-1]
+        b[i] = a[i-1]
+      
+    return a[n-1] + b[n-1]
+  
+
+if n<=0:
+    print('invalid',end="")
+
+else:
+    print(countStrings(n),end="")
+
+
+
+
