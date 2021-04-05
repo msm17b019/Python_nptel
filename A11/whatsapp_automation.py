77070f534ac4d4369f21163ca9443684a8144b0f
msm17b019 <sujeetkumariiit@gmail.com>
2021-04-05 12:35:04 +0530

    commit

--- A11/whatsapp_automation.py
+++ A11/whatsapp_automation.py
@@ -8,7 +8,7 @@ import time
 driver=webdriver.Chrome('C:\\Users\\sujee\\Downloads\\chromedriver_win32 (1)\\chromedriver')
 driver.get('https://web.whatsapp.com')
 wait=WebDriverWait(driver,600)
-target='"Rajeev"'
+target='"Naresh"'
 string="Automated message sent using python !!"
 x_arg='//span[contains(@title,'+target+')]'
 target=wait.until(EC.presence_of_element_located((By.XPATH, x_arg)))
@@ -17,5 +17,5 @@ target.click()
 input_box=driver.find_element_by_class_name('_2A8P4')
 
 
-for i in range(10):
+for i in range(0,100):
     input_box.send_keys(string+Keys.ENTER)
